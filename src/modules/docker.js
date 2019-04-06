cfbd1b0a0b80cdc884f534776103f3b9a465bd9f
Khizer Younas <m.khizeryounas@gmail.com>
2019-04-07 04:23:40 +0500

    removed env

--- src/modules/docker.js
+++ src/modules/docker.js
@@ -3,11 +3,19 @@ const { docker_sock } = require("../../config/keys");
 
 const docker = new Docker({ socketPath: docker_sock });
 
-const git_meta = {
-  repo:
-    "https://d1cfcd9e6ee418a96eb8c8a54d67e17aa8e9ab1b:x-oauth-basic@github.com/mkhizeryounas/express-mongo",
-  branch: "develop"
+const project = {
+  git_meta: {
+    repo:
+      "https://d1cfcd9e6ee418a96eb8c8a54d67e17aa8e9ab1b:x-oauth-basic@github.com/mkhizeryounas/express-mongo",
+    branch: "develop"
+  },
+  env: [
+    `mongodb="mongodb://mkhizeryounas:Mkhizer#321@ds159737.mlab.com:59737/dropspot"`
+  ]
 };
+
+project["envString"] = "";
+project.env.map(e => (project.envString += e + "\n"));
 
 docker.container
   .create({
@@ -16,10 +24,11 @@ docker.container
       "sh",
       "-c",
       `
-      git clone ${git_meta.repo} --single-branch --branch ${
-        git_meta.branch
+      git clone ${project.git_meta.repo} --single-branch --branch ${
+        project.git_meta.branch
       } app 
       cd app
+      echo ${project.envString} >> .env
       npm install
       npm start
       `
