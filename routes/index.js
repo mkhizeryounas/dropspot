76a766afa0ce6d8807ca20ef6c7af27ee071e1be
Khizer Younas <m.khizeryounas@gmail.com>
2019-04-23 02:48:37 +0500

    added pipeline modulte

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", async function(req, res, next) {
-  res.reply({ data: { title: "Express" } });
+  res.reply({ data: { title: "Dropspot CD Platform" } });
 });
 
 module.exports = router;
